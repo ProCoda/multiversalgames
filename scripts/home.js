c881cce02fd2a8760e47ff61f268c35588e8fd0f
DandyCoda <77184153+ProCoda@users.noreply.github.com>
2023-09-15 09:35:23 +1000

    commit 2

--- scripts/home.js
+++ scripts/home.js
@@ -8,7 +8,7 @@ var sticky = header.offsetTop;
 
 
 function myFunction() {
-  if (window.pageYOffset > sticky) {
+  if (window.scrollY > sticky) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
