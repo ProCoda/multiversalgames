48a41a78ef764efb926d29484fabbc04961d7ef5
ProCoda <77184153+ProCoda@users.noreply.github.com>
2023-09-26 10:19:11 +1000

    sticky header start

--- scripts/home.js
+++ scripts/home.js
@@ -1,19 +1,4 @@
-
-window.onscroll = function() {stickyFunction();};
-
-
-
-
-
-
-
-function stickyFunction() {
-  var header = document.getElementById("myHeader");
-  var sticky = header.offsetTop;
-  console.log(window.scrollY + " "+sticky)
-  if (window.scrollY > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
+window.addEventListener("scroll", function() {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0)
+})
