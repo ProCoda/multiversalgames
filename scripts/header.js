cb61dfe960aa470378ae50c49502b10468c79de7
ProCoda <77184153+ProCoda@users.noreply.github.com>
2023-09-28 12:11:49 +1000

    dropdown finish

--- scripts/header.js
+++ scripts/header.js
@@ -3,4 +3,18 @@ window.addEventListener("scroll", function() {
   header.classList.toggle("sticky", window.scrollY > 0)
 });
 
-const dropdownButton = document.getElementById("dropdownButton");
+document.addEventListener('click', e => {
+  const isDropdownButton = e.target.matches(".dropdownBtn");
+  if(!isDropdownButton && e.target.closest('.dropdown') != null) return;
+
+  let currentDropdown;
+  if(isDropdownButton){
+    currentDropdown = e.target.closest('.dropdown');
+    currentDropdown.classList.toggle('active');
+  }
+
+  document.querySelectorAll(".dropdown.active").forEach(dropdown => {
+    if(dropdown === currentDropdown) return;
+    dropdown.classList.remove("active");
+  })
+})
