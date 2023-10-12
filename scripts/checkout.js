ee3dc5b15403a73d6e79bebeba9b45869434ea63
ProCoda <77184153+ProCoda@users.noreply.github.com>
2023-10-12 13:03:47 +1100

    Co-authored-by: Maljupiter224 <Maljupiter224@users.noreply.github.com>

---
+++ scripts/checkout.js
@@ -0,0 +1,13 @@
+const gamesSelectors = document.getElementById('gameSelector');
+
+gamesSelectors.forEach((gameSelector) => {
+    gameSelector.addEventListener('click', function(){
+        console.log("breh");
+
+    });
+});
+
+// 1 game 9.99
+// 2 games 17.99
+// 3 games 26.99
+// 4 games 19.99
