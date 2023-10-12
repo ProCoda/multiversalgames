62c0b81d13a2320d28926208295cf1fdbd810e63
ProCoda <77184153+ProCoda@users.noreply.github.com>
2023-10-13 09:58:21 +1100

    checkout javascriopt

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,18 @@
-function selectorClick(num){
-    console.log(num)
+function selectorClick(name){
+    const selector = document.getElementById(name.trim() + "-select");
+    
+    if(selector.checked){
+        const container = document.getElementById('checkout-container');
+        let game = document.createElement('h3');
+        game.className = 'game-type';
+        game.innerHTML = '<span class="game-type-description">'+name+'</span><span class="game-type-amount">$9.99</span>'
+        container.prepend(game)
+    } else {
+        const games = document.querySelectorAll('.game-type');
+        games.forEach(function(game){
+            console.log(game.children.item(0))
+        })
+    }
 }
 
 
@@ -7,4 +20,4 @@ function selectorClick(num){
 // 1 game 9.99
 // 2 games 17.99
 // 3 games 26.99
-// 4 games 19.99
+// 4 games 34.99
