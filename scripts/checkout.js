e1d358dfd253e9aebb19baa11b75442617ad133d
ProCoda <77184153+ProCoda@users.noreply.github.com>
2023-10-16 15:20:14 +1100

    checkout error

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,43 +1,43 @@
-function selectorClick(name){
+function selectorClick(name) {
     var id = "";
-    if(name === "Artificial Apocalypse"){ id = "ArtificialApocalypse" }
-    if(name === "Avillius Adventure"){ id = "AvilliusAdventure" }
-    if(name === "Locked Laptop"){ id = "LockedLaptop" }
-    if(name === "Quantum Computer"){ id = "QuantumComputer" }
+    if (name === "Artificial Apocalypse") { id = "ArtificialApocalypse" }
+    if (name === "Avillius Adventure") { id = "AvilliusAdventure" }
+    if (name === "Locked Laptop") { id = "LockedLaptop" }
+    if (name === "Quantum Computer") { id = "QuantumComputer" }
     const selector = document.getElementById(id + "-select");
-    
-    if(selector.checked){
+
+    if (selector.checked) {
         const container = document.getElementById('checkout-container');
         let game = document.createElement('h3');
         game.className = 'game-type';
-        game.innerHTML = '<span class="game-type-description">'+name+'</span><span class="game-type-amount">$9.99</span>'
+        game.innerHTML = '<span class="game-type-description">' + name + '</span><span class="game-type-amount">$9.99</span>'
         container.prepend(game)
     } else {
         const games = document.querySelectorAll('.game-type');
-        games.forEach(function(game){
+        games.forEach(function (game) {
             var child = game.children.item(0);
-            if(child.textContent === name){
+            if (child.textContent === name) {
                 game.remove();
             }
-        })
+        });
     }
 
-    
+
 
     const games = document.querySelectorAll('.game-type');
     const total = document.getElementById('total-price');
     const discount = document.getElementById('discount-price');
 
     let discountPrice = 0;
-    if(games.length == 1){ discountPrice = 9.99 }
-    if(games.length == 2){ discountPrice = 16.99 }
-    if(games.length == 3){ discountPrice = 23.99 }
-    if(games.length == 4){ discountPrice = 30.99 }
+    if (games.length == 1) { discountPrice = 9.99 }
+    if (games.length == 2) { discountPrice = 16.99 }
+    if (games.length == 3) { discountPrice = 23.99 }
+    if (games.length == 4) { discountPrice = 30.99 }
 
     let totalPrice = games.length * 9.99;
 
-    total.innerText = "AUD $"+totalPrice;
-    discount.innerText = "AUD $"+discountPrice;
+    total.innerText = "AUD $" + totalPrice;
+    discount.innerText = "AUD $" + discountPrice;
 }
 
 
@@ -46,33 +46,39 @@ const modalConfirm = document.getElementById("confirm");
 const modalPayment = document.getElementById("payment");
 
 
-function checkoutClick(){
+function checkoutClick() {
     const games = document.getElementById('checkout-container').querySelectorAll(".game-type-description");
     const discount = document.getElementById('discount-price');
-    if(games.length == 0){  }
-    var str = "";
-    games.forEach((game) => {
-        str += game.innerHTML + ", "
-    });
-    str = str.substring(0, str.length-2);
-    modalConfirm.innerHTML = '<i><img src="images/cart.svg" style="width: 100px; height: auto"></i><h2>Confirm Purchase</h2><h3>Do you confirm the purchase of '+str+' for '+discount.innerText+'?</h3><div class="buttons"><button id="close-btn" onclick="closeModal()">No</button><button id="yes-btn" onclick="loadingModal()">Yes</button></div>';
-    modalPayment.classList.toggle('activeModal');
-    document.getElementById("blurWrapper").classList.toggle('blurred');
+    const errorMsg = document.getElementById('errorMsg');
+    if (games.length == 0) {
+        errorMsg.style.display = 'block'
+    } else {
+        var str = "";
+        games.forEach((game) => {
+            str += game.innerHTML + ", "
+        });
+        str = str.substring(0, str.length - 2);
+        modalConfirm.innerHTML = '<i><img src="images/cart.svg" style="width: 100px; height: auto"></i><h2>Confirm Purchase</h2><h3>Do you confirm the purchase of ' + str + ' for ' + discount.innerText + '?</h3><div class="buttons"><button id="close-btn" onclick="closeModal()">No</button><button id="yes-btn" onclick="loadingModal()">Yes</button></div>';
+        modalPayment.classList.toggle('activeModal');
+        document.getElementById("blurWrapper").classList.toggle('blurred');
+        errorMsg.style.display = 'none'
+    }
+
 }
 
-function closeModal(){
+function closeModal() {
     modalConfirm.classList.remove('activeModal');
     modalPayment.classList.remove('activeModal');
     document.getElementById("blurWrapper").classList.remove('blurred');
 }
 
-function confirm(){
+function confirm() {
     modalPayment.classList.remove('activeModal');
     modalConfirm.classList.toggle('activeModal');
 }
 
 
-function loadingModal(){
+function loadingModal() {
     const icon = modalConfirm.children.item(0);
     const title = modalConfirm.children.item(1);
     const text = modalConfirm.children.item(2);
